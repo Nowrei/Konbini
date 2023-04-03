b5d1d57e1089b7cd90903a9e6777728a65ed0ad2
nowrei <furlancedric@gmail.com>
2023-04-03 12:13:45 +0200

    Navbar+responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,24 @@ module.exports = {
   content: ['./templates/**/*.html.twig',
   './node_modules/tw-elements/dist/js/**/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+    
+        'white': '#ffffff',
+        'konbini': {
+          100: '#FFC800', // bleu
+          200: '#2A303B', // gris
+          300: '#F5F6F8', //gris clair
+          400: '#1E32C8',
+          500: '#ef6602',
+          600: '#000000'
+        },
+      },
+      height: {
+        '15': '80px',
+
+      },
+    },
   },
   plugins: [require("tw-elements/dist/plugin")],
 }
